b0b0f4424e541711e1bfa107fa6eb96455dda289
Kevin Yu <kevin.sang.yu@gmail.com>
2021-08-09 14:41:03 +1000

    first commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame();
+        frame.setTitle("Project Reminder");
+        frame.setSize(500, 500);
+        frame.setVisible(true);
+    }
+}
